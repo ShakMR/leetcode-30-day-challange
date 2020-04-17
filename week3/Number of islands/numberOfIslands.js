2a241db0f7808fef30e7fb942d8fe0d94f18879c
ShakMR <borja.arias.upc@gmail.com>
2020-04-18 01:53:19 +0200

    Number of islands finished

--- week3/Number of islands/numberOfIslands.js
+++ week3/Number of islands/numberOfIslands.js
@@ -1,7 +1,19 @@
-const printMap = grid => {
-  console.log('\n####################');
-  console.log(grid.join('\n'));
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+const printMap = (grid, nIslands) => {
+  function clearConsoleAndScrollbackBuffer() {
+    process.stdout.write("\u001b[3J\u001b[2J\u001b[1J");console.clear();
+  }
+  // clearConsoleAndScrollbackBuffer();
+  console.log('\n####################', nIslands);
+  console.log(grid.map(r => r.join('')).join('\n'));
   console.log('####################\n');
+  // sleep(100);
 }
 
 const changeGrid = (grid, i, j, value) => {
@@ -14,8 +26,10 @@ const getNextCoords = (cI, cJ, grid) => {
   const rowLength = grid[0].length;
   const columnLength = grid.length;
   const next = [];
+  if (cJ > 0) { next.push([cI, cJ - 1])}
+  if (cI > 0) { next.push([cI - 1, cJ])}
+  if (cJ < rowLength - 1) { next.push([cI, cJ + 1])}
   if (cI < columnLength - 1) { next.push([cI + 1, cJ])}
-  if (cJ < rowLength - 1) { next.push([cI, cJ+1])}
   return next;
 }
 
@@ -29,14 +43,18 @@ var numIslands = function(grid) {
   }
   let nIslands = 0;
   let next;
-  const nextCoords = [[0, 0]];
-  while (next = nextCoords.shift()) {
+  const nextLand = [[0, 0]];
+  const nextSea = [];
+  //let's follow the land first
+  let n = 0;
+  while (next = nextLand.shift() || nextSea.shift()) {
+    n++;
     const [i, j] = next;
     let current = grid[i][j];
     const adjacent = getNextCoords(i, j, grid);
 
     if (current === '0') {
-      grid[i][j] = '_'
+      grid[i][j] = ' '
     } else if (current === '1') {
       grid[i][j] = 'X' // visited and counted as island
       current = 'X';
@@ -45,11 +63,19 @@ var numIslands = function(grid) {
 
     adjacent.forEach(([x, y]) => {
       const nextValue = grid[x][y];
-      if (!['_', 'X'].includes(nextValue)) {
-        nextCoords.push([x,y])
-      }
-      if (nextValue === '1' && current === 'X') {
-        grid[x][y] = 'X';
+      switch (nextValue) {
+        case '1':
+          nextLand.unshift([x,y]);
+          if (current === 'X') {
+            grid[x][y] = 'X'; // mark island as counted
+          }
+          break;
+        case '0':
+          nextSea.push([x,y]);
+          grid[x][y] = ' '; // mark island as counted
+          break;
+        default:
+          //already visited, do nothing
       }
     });
   }
