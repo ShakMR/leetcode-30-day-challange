c75768b20f2e62693a0b1aedb8dbb84c8625fb0a
ShakMR <borja.arias.upc@gmail.com>
2020-05-02 19:31:01 +0200

    Improve test creation script

--- newProblem.sh
+++ newProblem.sh
@@ -1,5 +1,10 @@
 #!/bin/zsh
 
+readQ () {
+  echo "$1"
+  read -r "$2"
+}
+
 week=${1}
 problemNameWithSpaces=${@:2}
 
@@ -14,7 +19,13 @@ javascriptFile="$workdir/$problemNameCamelCase.js"
 testFile="$workdir/$problemNameCamelCase.test.js"
 echo "Creating files: ${javascriptFile}"
 echo "Creating files: ${testFile}"
-touch $javascriptFile
-touch $testFile
+touch "$javascriptFile"
+touch "$testFile"
 
-echo "const test = require('ava');" > ${testFile};
+readQ "Do you want to create a test? [Y/n]" createTest
+
+if [ "$createTest" == "" ] || [ "$createTest" == 'Y' ] || [ "$createTest" == 'y' ]; then
+  readQ "Give me the input:" input
+  readQ "Give me the expected output" output
+  sed 's/::input::/'${input}'/gm' < test_template.templ | sed 's/::output::/'${output}'/gm' | sed 's/::name::/'${problemNameCamelCase}'/gm' > "${testFile}"
+fi
