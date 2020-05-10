e6179bd0f0419fc86dfe760edb1dcf57273f7568
borjaarias <borja.arias@skyscanner.net>
2020-05-11 00:36:18 +0200

    problem 10th version 2

--- may/week2/Find the Town Judge/findTheTownJudge.js
+++ may/week2/Find the Town Judge/findTheTownJudge.js
@@ -1,53 +1,18 @@
-class Edge {
-  constructor(from, to) {
-    this.from = from;
-    this.to = to;
-  }
-}
-
-class Node {
-  constructor(id) {
-    this.id = id;
-    this.outgoingEdges = [];
-    this.incomingEdges = [];
-  }
-
-  addIncomingEdge(from) {
-    this.incomingEdges.push(from);
-  }
-
-  addOutgoingEdge(to) {
-    this.outgoingEdges.push(to);
-  }
-
-  getEdgesNumber() {
-    return {
-      outgoing: this.outgoingEdges.length,
-      incoming: this.incomingEdges.length,
-    };
-  }
-}
-
-const findTheTownJudge = (nPeople, trustArray) => {
-  const trustMap = new Map();
-  // build graph
-  let nodes = [];
-  for (let i = 1; i <= nPeople; i++) {
-    nodes.push(new Node(i));
-  }
+const findJudge = (nPeople, trustArray) => {
+  const canBeJudge = (new Array(nPeople)).fill(0);
 
   trustArray.forEach(([from, to]) => {
-    nodes[from-1].addOutgoingEdge(to);
-    nodes[to-1].addIncomingEdge(from);
+    canBeJudge[to-1]++;
+    canBeJudge[from-1] = -1;
   });
 
-  for (let i = 0; i < nPeople; i++) {
-    const { outgoing, incoming } = nodes[i].getEdgesNumber();
-    if (outgoing === 0 && incoming === nPeople-1) {
-      return i+1;
+  for (let i = 0; i < nPeople; ++i) {
+    const j = canBeJudge[i];
+    if (j === nPeople - 1) {
+      return i + 1;
     }
   }
   return -1;
 }
 
-module.exports = findTheTownJudge;
+module.exports = findJudge;
